03051d5cc93723fb9f27b1c45b0453db6dd4bd0d
André Almeida <andrevictor7777@gmail.com>
2024-06-24 16:27:48 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,11 @@
+const nome = 'André'
+const idade = 26
+const maiorDeIdade = true
+const frase = `Meu nome é ${nome},tenho ${idade} anos`
+if (idade >=18){
+    console.log('Maior de idade')
+} else{
+    console.log('Menor de idade')
+}
+const multiplicacao = (idade + 2)/2
+console.log(multiplicacao)
---
+++ ex1.js
@@ -0,0 +1,59 @@
+//Exercício 1
+
+const numero = 5
+const numero1 = 5
+const adicao = numero + numero1
+console.log('Exercicio 1: '+ adicao)
+
+//Exercício 2
+const numero2 = 5
+const numero3 = 10
+const numero4 = 15
+const media = (numero2 + numero3 +numero4) /3
+console.log('Exercicio 2: '+ media)
+
+//Exercício 3
+const numero5 = 1
+const numero6 = 5.4
+const multiplicacao =(numero5 * numero6)
+console.log('Exercicio 3: '+ multiplicacao)
+
+
+//Exercício 4
+const produto = 1000
+const desconto = 0.1
+const valordesconto = (produto * desconto)
+const  valorfinal = (produto - valordesconto)
+console.log('Exercicio 4: '+ valorfinal)
+
+//Exercício 5
+const salario = 1200
+const aumento = 0.1
+const aumentosalarial = (salario * aumento)
+const valorfinal2 = (aumentosalarial + salario)
+console.log('Exercicio 5: '+ valorfinal2)
+
+//Exercicio 6
+const base = 10
+const altura = 10
+const area = (base*altura)
+console.log('Exercicio 6: '+ area )
+
+//Exercicio 7
+const baseparede = 10
+const alturaparede = 2
+const areaparede = (baseparede * alturaparede )
+const litro = 2
+const valorfinal3 = (areaparede/litro)
+console.log('Exercicio 7: '+ valorfinal3)
+
+//Exercicio 8
+const diaria = 95
+const km = 0.5
+const dias = 30
+const kmrodado = 10000
+const valordiaria = (diaria * dias)
+const valorkilometragem = (km * kmrodado)
+const valorfinal4 = (valordiaria + valorkilometragem)
+console.log('Exercicio 8: '+valorfinal4)
+
